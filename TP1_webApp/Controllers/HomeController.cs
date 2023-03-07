0117ab45d28babd35355d55a2b263ff1d0810971
CNNunez <nnunez@ic-itcr.ac.cr>
2023-03-07 02:29:11 -0600

    create MVC web app; connect web app to DB; send request to DB; collect data from DB; still missing to show all the collected data on table

---
+++ TP1_webApp/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TP1_webApp.Models;
+
+namespace TP1_webApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // HomeController
+        private readonly ILogger<HomeController> _logger;
+
+
+        // Logger
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        // Idex view
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        // Privacy view
+        public IActionResult Privacy()
+        {
+            SQLConnection myServer = new SQLConnection();
+            // ... calling the model method
+            myServer.Get();
+            return View(myServer);
+        }
+
+        // Error view
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public ActionResult Get_Items()
+        {
+            SQLConnection myModel = new SQLConnection();
+            // ... calling the model method
+            myModel.Get();
+            return View("Privacy", myModel);
+        }
+    }
+}
---
+++ TP1_webApp/Models/SQLConnection.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+
+
+namespace TP1_webApp.Models
+{
+
+    public class SQLConnection
+    {
+        // Attribute
+        public List<ItemClass> ItemsList = new List<ItemClass>();
+
+
+        // Init
+        // ... this inicialize the table
+        public SQLConnection()
+        {
+            ItemClass articuloInfo = new ItemClass("NA1", "NA2", "NA3");
+            ItemsList.Add(articuloInfo);
+        }
+
+
+        // Method
+        // ... getting info from DB
+        public void Get()
+        {
+            try
+            {
+                // ... DB credentials
+                String DBCredentials = "Data Source=ec2-3-144-162-161.us-east-2.compute.amazonaws.com;Initial Catalog=TP1;Persist Security Info=True;User ID=sa;Password=Admin1234";
+                SqlConnection Connection = new SqlConnection(DBCredentials);
+                
+                // ... open connection, send request and read responce
+                Connection.Open();
+                SqlCommand SelectCommand = new SqlCommand("SELECT * FROM dbo.Articulo", Connection);
+                SqlDataReader Reader = SelectCommand.ExecuteReader();
+                
+                // ... collect the items from the DB
+                while (Reader.Read())
+                {
+                    String articuloID = Reader["Id"].ToString();
+                    String articuloName = Reader["Nombre"].ToString();
+                    String articuloPrice = Reader["Precio"].ToString();
+
+                    ItemClass articuloInfo = new ItemClass(articuloID, articuloName, articuloPrice);
+                    ItemsList.Add(articuloInfo);
+                }
+
+                // ... close connection
+                Reader.Close();
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
+        }
+    }
+}
